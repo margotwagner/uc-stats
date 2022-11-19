3cf8ed808e13cfdffa58627e2ca20932b693b759
Margot Wagner <39107794+margotwagner@users.noreply.github.com>
2022-11-19 14:44:33 -0800

    initialized git repo using CookieCutter Data Science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Statistical analysis of University of California over time. Current analyses focuses on UC San Diego ass a case study and looks at finances, housing, enrollment, compensation, and employee breakdowns. ',
+    author='Margot Wagner',
+    license='MIT',
+)
